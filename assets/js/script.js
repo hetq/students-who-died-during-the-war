08d7b7303ec7eadb7787960ac67c46d7f13c03e4
sandaramet <aram@gratun.am>
2021-11-18 13:18:32 +0400

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,26 @@
-  let start = 0;
-  let end = 40;
-  createStudentsBlock(start, end);
-  let isLoading = false;
-  $(window).on("scroll", () => {
-    if (isLoading == false) {
-      if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-        start += 40;
-        end += 40;
-        createStudentsBlock(start, end);
-      }
+let start = 0;
+let end = 40;
+createStudentsBlock(start, end);
+let isLoading = false;
+$(window).on("scroll", () => {
+  if (isLoading == false) {
+    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+      start += 40;
+      end += 40;
+      createStudentsBlock(start, end);
     }
-  });
+  }
+});
 function createStudentsBlock(start, end) {
   for (let i = start; i < end; i++) {
     const student = DATA[i];
-    const course =
-      student.course == 1 ? `${student.course}-ին` : `${student.course}-րդ`;
+    let course;
+    if (student.course == 1)
+      course = `${student.course}-ին`
+    else if (student.course == 0)
+      course = `${student.course}`
+    else
+      course = `${student.course}-րդ`
     let imageLink = `${student.name.replace(/\s+/g, " ").trim()}.jpg`;
     const image = isImageExists(`assets/img_/${imageLink}`)
       ? imageLink
@@ -28,9 +33,13 @@ function createStudentsBlock(start, end) {
                       <h3>${student.name}</h3>
                       <p> 
                           <span>Սովորել է</span> 
-                          <span>${student.educationalInstitution}ի</span> 
-                          <span class="faculty">${student.faculty}ի</span> 
-                          <span>${course} կուրսում</span>
+                          <span>
+                            ${student.educationalInstitution}
+                             ${student.course ? 'ի' : ''}
+                          </span> 
+                          ${student.course ? `<span class="faculty">${student.faculty}ի</span> 
+                          <span>${course} կուրսում</span>` : ''}
+                          
                       </p>
                   </div>
                   <div 
